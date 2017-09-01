e182efe3cfd49b95dd621b857885d8b3acd4d5f2
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-09-01 16:19:33 -0400

    testing

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/finalexam/controller/NavDrawerAdapter.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/finalexam/controller/NavDrawerAdapter.java
@@ -1,9 +1,11 @@
 package nyc.c4q.shannonalexander_navarro.finalexam.controller;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +18,10 @@ import nyc.c4q.shannonalexander_navarro.finalexam.views.NavDrawerViewHolder;
  * Created by shannonalexander-navarro on 2/12/17.
  */
 
-public class NavDrawerAdapter extends RecyclerView.Adapter {
+public class NavDrawerAdapter extends RecyclerView.Adapter  implements View.OnClickListener{
     private List<NavigationItem> navItems = new ArrayList<>();
 
+    Context context;
     public NavDrawerAdapter(List<NavigationItem> navigationItems) {
         this.navItems = navigationItems;
     }
@@ -26,6 +29,7 @@ public class NavDrawerAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.nav_list_item, parent, false);
+        context = itemView.getContext();
         return new NavDrawerViewHolder(itemView);
     }
 
@@ -35,10 +39,25 @@ public class NavDrawerAdapter extends RecyclerView.Adapter {
         NavDrawerViewHolder navigationDrawerViewHolder = (NavDrawerViewHolder) holder;
         NavigationItem aNavItem = navItems.get(position);
         navigationDrawerViewHolder.bind(aNavItem);
+
+        if (aNavItem.geticonText() == "Button 1"){
+
+
+        }
+        if (aNavItem.geticonText()=="Button 2") {
+
+        }
     }
 
     @Override
     public int getItemCount() {
         return 4;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case 0: Toast.makeText(context, "Selected Heart Activity", Toast.LENGTH_SHORT).show();
+        }
     }
 }
